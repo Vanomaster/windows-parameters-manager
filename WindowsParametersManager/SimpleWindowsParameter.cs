8900211d2617fd6e460cd27b614c15abee113f42
Vanous <https://github.com/Vanomaster/blum-blum-shuba-algorithm.git>
2022-03-29 23:23:53 +0700

    Init

---
+++ WindowsParametersManager/SimpleWindowsParameter.cs
@@ -0,0 +1,94 @@
+﻿namespace ParametersManager;
+
+/// <summary>
+/// Simple parameter in Windows OS.
+/// </summary>
+public class SimpleWindowsParameter
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SimpleWindowsParameter"/> class.
+    /// </summary>
+    /// <param name="path">Path of registry parameter.</param>
+    /// <param name="name">Name of registry parameter.</param>
+    public SimpleWindowsParameter(string? path, string? name)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            throw new ArgumentException("An empty path was passed!");
+        }
+
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("An empty name was passed!");
+        }
+
+        Path = path;
+        Name = name;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SimpleWindowsParameter"/> class.
+    /// </summary>
+    /// <param name="name">Name of registry parameter.</param>
+    /// <param name="value">Value of registry parameter.</param>
+    public SimpleWindowsParameter(string? name, object? value)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("An empty name was passed!");
+        }
+
+        if (value == null)
+        {
+            throw new ArgumentException("An empty value was passed!");
+        }
+
+        Name = name;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SimpleWindowsParameter"/> class.
+    /// </summary>
+    /// <param name="path">Path of registry parameter.</param>
+    /// <param name="name">Name of registry parameter.</param>
+    /// <param name="value">Value of registry parameter.</param>
+    public SimpleWindowsParameter(string? path, string? name, object? value)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            throw new ArgumentException("An empty path was passed!");
+        }
+
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentException("An empty name was passed!");
+        }
+
+        if (value == null)
+        {
+            throw new ArgumentException("An empty value was passed!");
+        }
+
+        Path = path;
+        Name = name;
+        Value = value;
+    }
+
+    /// <summary>
+    /// Path of registry parameter.
+    /// </summary>
+    public string? Path { get; protected set; }
+
+    /// <summary>
+    /// Name of registry parameter.
+    /// </summary>
+    public string? Name { get; protected set; }
+
+    /// <summary>
+    /// Value of registry parameter.
+    /// </summary>
+    public object? Value { get; protected set; }
+
+    // TODO Defining parameter type
+}
---
+++ WindowsParametersManager/WindowsParameter.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.Win32;
+
+namespace ParametersManager;
+
+/// <summary>
+/// Parameter in Windows OS.
+/// </summary>
+public class WindowsParameter : SimpleWindowsParameter
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowsParameter"/> class.
+    /// </summary>
+    /// <param name="path">Path of registry parameter.</param>
+    /// <param name="name">Name of registry parameter.</param>
+    public WindowsParameter(string? path, string? name)
+        : base(path, name)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowsParameter"/> class.
+    /// </summary>
+    /// <param name="name">Name of registry parameter.</param>
+    /// <param name="value">Value of registry parameter.</param>
+    public WindowsParameter(string? name, object? value)
+        : base(name, value)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowsParameter"/> class.
+    /// </summary>
+    /// <param name="path">Path of registry parameter.</param>
+    /// <param name="name">Name of registry parameter.</param>
+    /// <param name="value">Value of registry parameter.</param>
+    public WindowsParameter(string? path, string? name, object? value)
+        : base(path, name, value)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowsParameter"/> class.
+    /// </summary>
+    /// <param name="path">Path of registry parameter.</param>
+    /// <param name="name">Name of registry parameter.</param>
+    /// <param name="value">Value of registry parameter.</param>
+    /// <param name="additions">Additions of registry parameter.</param>
+    public WindowsParameter(string? path, string? name, object? value, List<SimpleWindowsParameter> additions)
+        : base(path, name, value)
+    {
+        if (additions == null)
+        {
+            throw new ArgumentException("An empty additions was passed!");
+        }
+
+        Additions = additions;
+    }
+
+    /// <summary>
+    /// Additional registry parameters required for this registry parameter.
+    /// </summary>
+    public List<SimpleWindowsParameter> Additions { get; } = new ();
+
+    // TODO different paths of different parameters
+
+    /// <summary>
+    /// Get registry parameter value.
+    /// </summary>
+    /// <returns>Registry parameter value.</returns>
+    public object? GetRegistryValue()
+    {
+        var key = Registry.CurrentUser.OpenSubKey(Path!);
+        Value = key?.GetValue(Name);
+
+        return Value;
+    }
+
+    /// <summary>
+    /// Get additional registry parameters required for this registry parameter.
+    /// </summary>
+    /// /// <param name="additionsPath">Additions path.</param>
+    /// <exception cref="ArgumentNullException">Additions path is invalid.</exception>
+    /// <returns>Additional registry parameters required for this registry parameter.</returns>
+    public List<SimpleWindowsParameter> GetRegistryAdditions(string? additionsPath)
+    {
+        if (string.IsNullOrEmpty(additionsPath))
+        {
+            throw new ArgumentException("An empty additions path!");
+        }
+
+        var key = Registry.CurrentUser.OpenSubKey(additionsPath);
+        var detailsNames = key?.GetValueNames().ToList();
+        detailsNames?.ForEach(detailName => Additions
+            .Add(new WindowsParameter(additionsPath, detailName, key?.GetValue(detailName))));
+
+        return Additions;
+    }
+
+    /// <summary>
+    /// Set registry parameter value.
+    /// </summary>
+    /// <param name="newValue">New registry parameter value.</param>
+    /// <exception cref="ArgumentException">New value is invalid.</exception>
+    public void SetRegistryValue(string? newValue)
+    {
+        if (string.IsNullOrEmpty(newValue))
+        {
+            throw new ArgumentException("An empty new value was passed!");
+        }
+
+        var key = Registry.CurrentUser.OpenSubKey(Path!, true);
+        key?.SetValue(Name, newValue);
+
+        GetRegistryValue();
+    }
+
+    /// <summary>
+    /// Set additional registry parameters required for this registry parameter.
+    /// </summary>
+    /// <param name="additions">Additional registry parameters required for this registry parameter.</param>
+    /// <exception cref="ArgumentException">Additions or path to additions is invalid.</exception>
+    public void SetRegistryAdditions(List<SimpleWindowsParameter> additions)
+    {
+        if (additions == null)
+        {
+            throw new ArgumentException("An empty additions was passed!");
+        }
+
+        string? additionsPath = additions[0].Path;
+
+        if (string.IsNullOrEmpty(additionsPath))
+        {
+            throw new ArgumentException("An empty path to additions was passed!");
+        }
+
+        var key = Registry.CurrentUser.OpenSubKey(additionsPath, true);
+        additions.ForEach(parameter => key?.SetValue(parameter.Name, parameter.Value!));
+
+        GetRegistryAdditions(additionsPath);
+    }
+
+    /// <summary>
+    /// Create registry parameter value.
+    /// </summary>
+    /// <param name="value">Value of registry parameter.</param>
+    /// <exception cref="ArgumentException">Value is invalid.</exception>
+    public void CreateRegistryParameter(string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentException("An empty value was passed!");
+        }
+
+        var key = Registry.CurrentUser.CreateSubKey(Path!, true);
+        key.SetValue(Name, value);
+
+        GetRegistryValue();
+    }
+
+    /// <summary>
+    /// Create additional registry parameters required for this registry parameter.
+    /// </summary>
+    /// <param name="additions">Additional registry parameters required for this registry parameter.</param>
+    /// <exception cref="ArgumentException">Additions or path to additions is invalid.</exception>
+    public void CreateRegistryAdditions(List<SimpleWindowsParameter> additions)
+    {
+        if (additions == null)
+        {
+            throw new ArgumentException("An empty additions was passed!");
+        }
+
+        string? additionsPath = additions[0].Path;
+
+        if (string.IsNullOrEmpty(additionsPath))
+        {
+            throw new ArgumentException("An empty path to additions was passed!");
+        }
+
+        var key = Registry.CurrentUser.CreateSubKey(additionsPath, true);
+        additions.ForEach(parameter => key.SetValue(parameter.Name, parameter.Value!));
+
+        GetRegistryAdditions(additionsPath);
+    }
+}
